48240fd6a56a0427c2083b86b7140cb0b0a9eaa5
Naomi123-design <naomiokoh17@gmail.com>
2025-08-23 10:32:45 -0700

    DOM in js

---
+++ Javascript/Myscript.js
@@ -0,0 +1,8 @@
+document.getElementById("demo").innerHTML = "The abbreviation for Document object model is DOM";
+document.write("<br>")
+document.write("This is an external js file linked to a html file")
+function multiply(){
+    var num1=12
+    var num2=2
+    document.getElementById("myId").innerHTML = num1 * num2
+}
